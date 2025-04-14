d2584d04ff3881a9ce1a0b4a3b67f88366ba6c30
M.Bonheur <mbonheurc23@gmail.com>
2025-04-14 16:52:42 +0200

    updated current library_mis

--- view/src/app/page.tsx
+++ view/src/app/page.tsx
@@ -44,6 +44,7 @@ export default function Home() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
+        credentials:"include"
       });
 
       const data = await res.json();
